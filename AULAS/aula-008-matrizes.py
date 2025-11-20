c623b694453eac8f945aed03c73030d9e7b1944f
aluannarocha <luanna_pink@hotmail.com>
2025-11-20 17:02:43 +0000

    atualização aulas

---
+++ AULAS/aula-008-matrizes.py
@@ -0,0 +1,14 @@
+aluno1 = ['Telmo', 14]
+aluno2 = ['Solinho', 17]
+aluno3 = ['Pedro', 16]
+aluno4 = ['Leticia', 15]
+
+turma = list()
+
+turma.append(aluno1[:])
+turma.append(aluno2[:])
+turma.append(aluno3[:])
+turma.append(aluno4[:])
+
+for aluno in turma:
+    print(f'O/A aluno/a {aluno[0]} tem uma média de {aluno[1]} valores.')
---
+++ AULAS/aula-008-revisões.py
@@ -0,0 +1,4 @@
+aluno = [1, 5, 7, 6, 3, 'Ricardo']
+
+for indice, valor in enumerate(aluno):
+    print(f'No índice {indice} tem o valor {valor}')
